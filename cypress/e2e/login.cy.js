7578e48944dbdd97492e7cbc4fd3168000501ca5
Lucas Rezende <lucasrezendelrs@gmail.com>
2024-01-23 16:30:30 -0300

    Implementado o contexto de usuário bom, mas senha incorreta.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -19,4 +19,27 @@ describe('Login', function () {
       dashPage.header.userLoggedIn(user.name)
     })
   })
+  context('Quando o usuário é bom, mas a senha está incorreta', function () {
+    let user = {
+      name: 'Celso kamura',
+      email: 'kamura@samuraibs.com',
+      password: 'pwd123',
+      is_provider: true,
+    }
+    before(function () {
+      //Função(then) de callback para inserir a outra senha de forme procedural
+      cy.postUser(user).then(function () {
+        user.password = 'abc123'
+      })
+    })
+    it('Deve notificar erro de credenciais', function () {
+      loginPage.go()
+      loginPage.form(user)
+      loginPage.submit()
+
+      const message =
+        'Ocorreu um erro ao fazer login, verifique suas credenciais.'
+      loginPage.toast.shouldHaveText(message)
+    })
+  })
 })

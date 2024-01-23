10edbdbbc2e5bea5d5a3e0bacc7c133e4e52412c
Lucas Rezende <lucasrezendelrs@gmail.com>
2024-01-23 11:01:54 -0300

    Adiciona uso de 'before' para preparação de dados nos testes. Esse commit implementa o uso de um bloco 'before' nos testes, permitindo a remo ção e adição controlada de massa de dados antes da execução do testes. Isso mel hora a confiabilidade e a previsibilidade do testes, garantindo que os dados es tejam em um estado adequado para os testes.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,19 @@ describe('Login', function () {
       name: 'Robson Jassa',
       email: 'jassa@samuraibs.com',
       password: 'pwd123',
+      is_provider: true,
     }
+    before(function () {
+      cy.task('removeUser', user.email).then(function (result) {
+        console.log(result)
+      })
+      // Cadastra via API para garantir a massa de teste
+      cy.request('POST', 'http://localhost:3333/users', user).then(function (
+        response
+      ) {
+        expect(response.status).to.eq(200)
+      })
+    })
     it('Deve logar com sucesso', function () {
       loginPage.go()
       loginPage.form(user)

578d6915d66245482bd7df78e66c1b1363eb6f40
Lucas Rezende <lucasrezendelrs@gmail.com>
2024-01-24 14:49:43 -0300

    Implementado o contexto de recuperação de senha

--- cypress/support/components/toast/index.js
+++ cypress/support/components/toast/index.js
@@ -2,7 +2,7 @@ import { el } from './elements'
 class Toast {
   shouldHaveText(expectText) {
     // validação do resultado esperado
-    cy.get(el.toast)
+    cy.get(el.toast, { timeout: 10000 })
       .should('be.visible')
       .find('p')
       .should('have.text', expectText)

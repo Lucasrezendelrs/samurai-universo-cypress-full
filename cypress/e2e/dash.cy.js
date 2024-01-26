7fef4ed517bdbe906e032e3c37e7e9ca36a15077
Lucas Rezende <lucasrezendelrs@gmail.com>
2024-01-26 16:12:31 -0300

    Ajustes importantes (Guide Line)

--- cypress/e2e/dash.cy.js
+++ cypress/e2e/dash.cy.js
@@ -1,35 +1,23 @@
 import logiPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
+
+import { customer, provider, appointment } from '../support/factories/dash'
+
 describe('dashboard', function () {
   context('Quando o cliente faz um agendamento no app mobile', function () {
-    const data = {
-      customer: {
-        name: 'Cliente Teste',
-        email: 'clienteteste@cliente.com',
-        password: 'pwd123',
-        is_provider: false,
-      },
-      provider: {
-        name: 'Samurai Teste',
-        email: 'samuraiteste@profissional.com',
-        password: 'pwd456',
-        is_provider: true,
-      },
-      appointmentHour: '14:00',
-    }
     before(function () {
-      cy.postUser(data.provider)
-      cy.postUser(data.customer)
+      cy.postUser(provider)
+      cy.postUser(customer)
 
-      cy.apiLogin(data.customer)
+      cy.apiLogin(customer)
       //cy.log('Conseguimos pegar o token ' + Cypress.env('apiToken'))
 
-      cy.setProviderId(data.provider.email)
-      cy.createAppointment(data.appointmentHour)
+      cy.setProviderId(provider.email)
+      cy.createAppointment(appointment.hour)
     })
     it('O mesmo deve ser exibido no dashboard', function () {
       logiPage.go()
-      logiPage.form(data.provider)
+      logiPage.form(provider)
       logiPage.submit()
 
       dashPage.calenderShouldBeVisible()
@@ -37,7 +25,7 @@ describe('dashboard', function () {
       const day = Cypress.env('appointmentDay')
       dashPage.selectDay(day)
 
-      dashPage.appointmentShouldBe(data.customer, data.appointmentHour)
+      dashPage.appointmentShouldBe(customer, appointment.hour)
     })
   })
 })
---
+++ cypress/support/factories/dash/index.js
@@ -0,0 +1,15 @@
+exports.customer = {
+  name: 'Cliente Teste',
+  email: 'clienteteste@cliente.com',
+  password: 'pwd123',
+  is_provider: false,
+}
+exports.provider = {
+  name: 'Samurai Teste',
+  email: 'samuraiteste@profissional.com',
+  password: 'pwd456',
+  is_provider: true,
+}
+exports.appointment = {
+  hour: '14:00',
+}
